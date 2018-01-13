93b55b8c42f1a74ec9e47a3dea5e3a8970011b23
Sherry El Garhy <sherryptk@gmail.com>
2018-01-13 20:55:49 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,17 +9,28 @@ class App < Sinatra::Base
     end
 
     post '/teams' do
-      @team_name = params["team"]["name"]
-      @team_motto = params["team"]["motto"]
-      @hero_name = params["team"]["heroes"][0]["name"]
-      @hero_power = params["team"]["heroes"][0]["power"]
-      @hero_bio = params["team"]["heroes"][0]["bio"]
-      @hero_name2 = params["team"]["heroes"][1]["name"]
-      @hero_power2 = params["team"]["heroes"][1]["power"]
-      @hero_bio2 = params["team"]["heroes"][1]["bio"]
-      @hero_name3 = params["team"]["heroes"][2]["name"]
-      @hero_power3 = params["team"]["heroes"][2]["power"]
-      @hero_bio3 = params["team"]["heroes"][2]["bio"]
+      @team_name = params[:team][:name]
+      @team_motto = params[:team][:motto]
+      @hero_name = []
+      @hero_power = []
+      @hero_bio = []
+      @team_members = params[:team][:heroes]
+      @team_members.each do |hero|
+        @hero_name << hero[:name]
+        @hero_power << hero[:power]
+        @hero_bio << hero[:bio]
+      end
+      # @team_name = params["team"]["name"]
+      # @team_motto = params["team"]["motto"]
+      # @hero_name = params["team"]["heroes"][0]["name"]
+      # @hero_power = params["team"]["heroes"][0]["power"]
+      # @hero_bio = params["team"]["heroes"][0]["bio"]
+      # @hero_name2 = params["team"]["heroes"][1]["name"]
+      # @hero_power2 = params["team"]["heroes"][1]["power"]
+      # @hero_bio2 = params["team"]["heroes"][1]["bio"]
+      # @hero_name3 = params["team"]["heroes"][2]["name"]
+      # @hero_power3 = params["team"]["heroes"][2]["power"]
+      # @hero_bio3 = params["team"]["heroes"][2]["bio"]
       erb :team
     end
 
