d96dd8f6d78218aed9d10c84e340dc46a9ac6eb3
Sherry El Garhy <sherryptk@gmail.com>
2018-01-13 21:01:09 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,17 +20,7 @@ class App < Sinatra::Base
         @hero_power << hero["power"]
         @hero_bio << hero["bio"]
       end
-      # @team_name = params["team"]["name"]
-      # @team_motto = params["team"]["motto"]
-      # @hero_name = params["team"]["heroes"][0]["name"]
-      # @hero_power = params["team"]["heroes"][0]["power"]
-      # @hero_bio = params["team"]["heroes"][0]["bio"]
-      # @hero_name2 = params["team"]["heroes"][1]["name"]
-      # @hero_power2 = params["team"]["heroes"][1]["power"]
-      # @hero_bio2 = params["team"]["heroes"][1]["bio"]
-      # @hero_name3 = params["team"]["heroes"][2]["name"]
-      # @hero_power3 = params["team"]["heroes"][2]["power"]
-      # @hero_bio3 = params["team"]["heroes"][2]["bio"]
+  
       erb :team
     end
 
