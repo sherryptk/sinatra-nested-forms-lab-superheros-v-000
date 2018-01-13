3c661a0eff922ea8f9b7d74e99cd88a09c5cc69d
Sherry El Garhy <sherryptk@gmail.com>
2018-01-13 20:58:50 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,16 +9,16 @@ class App < Sinatra::Base
     end
 
     post '/teams' do
-      @team_name = params[:team][:name]
-      @team_motto = params[:team][:motto]
+      @team_name = params["team"]["name"]
+      @team_motto = params["team"]["motto"]
       @hero_name = []
       @hero_power = []
       @hero_bio = []
-      @team_members = params[:team][:heroes]
+      @team_members = params["team"]["heroes"]
       @team_members.each do |hero|
-        @hero_name << hero[:name]
-        @hero_power << hero[:power]
-        @hero_bio << hero[:bio]
+        @hero_name << hero["name"]
+        @hero_power << hero["power"]
+        @hero_bio << hero["bio"]
       end
       # @team_name = params["team"]["name"]
       # @team_motto = params["team"]["motto"]
